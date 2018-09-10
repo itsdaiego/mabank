7e3f73b0cb3f44bb553834c989f97f6e2a42b2b6
Dyego Rodrigo <dyego9444@gmail.com>
2018-09-10 01:10:54 -0300

    fix: logging
    remove exclusion from logback-classic
    add logs folder

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [com.datomic/datomic-free "0.9.5697"]
                  [io.pedestal/pedestal.service "0.5.2"]
                  [io.pedestal/pedestal.jetty "0.5.2"]
-                 [ch.qos.logback/logback-classic "1.1.8" :exclusions [org.slf4j/slf4j-api]]
+                 [ch.qos.logback/logback-classic "1.1.8"]
                  [org.slf4j/jul-to-slf4j "1.7.22"]
                  [org.slf4j/jcl-over-slf4j "1.7.22"]
                  [org.slf4j/log4j-over-slf4j "1.7.22"]
