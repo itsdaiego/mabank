b4283aea919b1f1bd4c63c6b400596917db0e831
Dyego Lima <dyego9444@gmail.com>
2019-04-14 21:57:31 -0300

    balance: create

---
+++ src/mabank/models/balance/create.clj
@@ -0,0 +1,17 @@
+(ns mabank.models.balance.create
+  (:require [mabank.db :as db]
+            [datomic.api :as d]
+            [cheshire.core :refer :all]))
+
+(defn save
+  [payables]
+  (map #(let [tx-result @(d/transact db/conn [{:db/id (d/tempid :db.part/user)
+                                         :balance/status "future-payment"
+                                         :balance/amount (get % :amount)
+                                         :balance/fee (get % :fee)
+                                         :balance/recipient (get % :recipient-id)
+                                         :balance/payable (get % :payable-id)
+                                         }])]) payables))
+(defn run
+  [model payable]
+  (save payable))
--- src/mabank/models/payable/create.clj
+++ src/mabank/models/payable/create.clj
@@ -1,17 +1,23 @@
 (ns mabank.models.payable.create
   (:require [mabank.db :as db]
+            [mabank.models.balance.create :as balance-create]
             [datomic.api :as d]
             [cheshire.core :refer :all]
             [clj-time.core :as time-core]
             [clj-time.coerce :as time-coerce]
-            [clj-time.format :as time-format]))
+            [clj-time.format :as time-format]
+            [robert.hooke :as hooke]))
 
+
+(def mdr-rate 0.05)
+
+(def default-status "waiting-payemnt")
+
+(def new-id (d/tempid :db.part/user))
 
 (defn calculate-amount
   [transaction]
   (/ (get transaction :amount) (get transaction :installments)))
-
-(def mdr-rate 0.05)
 
 (defn calculate-fee
   [payable-amount]
@@ -23,7 +29,7 @@
                     (time-format/formatters :year-month-day) (str (time-core/today)))
                   (time-core/days (* 30 installment)))))
 
-(def default-status "waiting-payemnt")
+
 
 (defn save
   [transaction current-installment]
@@ -33,22 +39,36 @@
                           :installment current-installment
                           :payment-date (calculate-payment-date current-installment)
                           :recipient-id (get transaction :recipient-id)
-                          :transaction-id (get transaction :transaction-id))]
+                          :transaction-id (get transaction :transaction-id))
 
-    @(d/transact db/conn [{:db/id (d/tempid :db.part/user)
-                           :payable/amount (get payable :amount)
-                           :payable/fee (get payable :fee)
-                           :payable/installment (get payable :installment)
-                           :payable/payment-date (get payable :payment-date)
-                           :payable/recipient (get payable :recipient-id)
-                           :payable/transaction (get payable :transaction-id)
-                           :payable/status default-status
-                           }])))
+        tx-result @(d/transact db/conn [{:db/id new-id
+                                          :payable/amount (get payable :amount)
+                                          :payable/fee (get payable :fee)
+                                          :payable/installment (get payable :installment)
+                                          :payable/payment-date (get payable :payment-date)
+                                          :payable/recipient (get payable :recipient-id)
+                                          :payable/transaction (get payable :transaction-id)
+                                          :payable/status default-status
+                                          }])]
+
+    (assoc payable  :payable-id (d/resolve-tempid (:db-after tx-result) 
+                                                  (:tempids tx-result) new-id))))
+
+(defn build
+  [payable]
+  ())
+
+
+(defn save-payables
+  [transaction]
+  (doall (map (partial save transaction)
+              (drop 1 (range (inc (get transaction :installments)))))))
 
 (defn run
-  [tx-result transaction]
-   (doall (map (partial save transaction) 
-       (drop 1 (range (inc (get transaction :installments))))))
+  [model transaction]
+  (-> (save-payables transaction)
+      (build))
+  (generate-string transaction))
 
-   ;; TODO: find better way of dealing with add-hook function
-   (generate-string transaction))
+;; TODO fix hooke args
+(hooke/add-hook #'build #'balance-create/run)
