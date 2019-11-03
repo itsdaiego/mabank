cfd0399e0d9aeee6d51b3ea7a61b9cb2bbb37e25
Dyego Lima <dyego9444@gmail.com>
2019-11-03 19:05:59 -0300

    transaction: fix integration tests

--- test/mabank/models/transaction/fetch_test.clj
+++ test/mabank/models/transaction/fetch_test.clj
@@ -4,7 +4,8 @@
             [datomic.api :as d]
             [mabank.models.helper.recipient-test :as mock-recipient]
             [mabank.models.helper.transaction-test :as mock-transaction]
-            [mabank.models.transaction.fetch :refer :all]))
+            [mabank.models.transaction.fetch :refer :all]
+            [cheshire.core :refer :all]))
 
 (defn create-empty-db
   []
@@ -18,9 +19,17 @@
 
 (def fake-conn (create-empty-db))
 
-(expect "[{\"id\":17592186045420,\"amount\":100,\"installments\":1,\"recipient-id\":17592186045418,\"status\":\"waiting-payemnt\"}]"
+(defn parse-response
+  [response]
+  (let [parsed-response (parse-string response true)]
+    (hash-map :amount (:amount (first parsed-response))
+              :installments (:installments (first parsed-response))
+              :status (:status (first parsed-response))
+              :recipient-id (:recipient-id (first parsed-response)))))
+
+(expect {:amount 100 :installments 1 :recipient-id 123 :status "waiting-payemnt"}
 		(with-redefs [conn fake-conn]
 		  (do
-            (let [recipient-id (get (mock-recipient/create fake-conn) :recipient-id)]
-              (mock-transaction/create recipient-id fake-conn))
-            (run))))
+            (mock-recipient/create fake-conn)
+            (mock-transaction/create fake-conn)
+            (parse-response (run)))))
